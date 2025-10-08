31486295d1b507aa9836f900472fc95a6fb6427a
Nimsara Thamel <nimsarathamel@gmail.com>
2025-10-08 23:12:12 +0530

    Add UpdateRead component and routing to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Write from "./components/Write";
 import Read from "./components/Read";
+import UpdateRead from "./components/UpdateRead";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
           <Route path="/" element={<Write />} />
           <Route path="/write" element={<Write />} />
           <Route path="/read" element={<Read />} />
+          <Route path="/updateRead" element={<UpdateRead />} />
         </Routes>
       </Router>
     </div>
