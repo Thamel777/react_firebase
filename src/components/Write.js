19087ee24edf0d9aba5871baa0159f19352e5569
Nimsara Thamel <nimsarathamel@gmail.com>
2025-10-09 00:59:12 +0530

    Add UpdateWrite component and enhance routing in App.js; update Read and Write components for better navigation and data handling

--- src/components/Write.js
+++ src/components/Write.js
@@ -1,33 +1,46 @@
 import React, {useState} from 'react';
 import app from "../firebaseConfig";
-import { getDatabase, ref, set, push } from 'firebase/database';
+import { getDatabase, ref, set, push } from "firebase/database";
+import { useNavigate } from 'react-router-dom';
 
 function Write() {
-  const [inputValue1, setInputValue1] = useState('');
-  const [inputValue2, setInputValue2] = useState('');
 
-  const saveData = async() => {
+  const navigate = useNavigate();
+
+  let [inputValue1, setInputValue1] = useState("");
+  let [inputValue2, setInputValue2] = useState("");
+
+  const saveData = async () => {
     const db = getDatabase(app);
-    const newDocRef = push(ref(db, 'nature/fruits'));
-    set (newDocRef, {
+    const newDocRef = push(ref(db, "nature/fruits"));
+    set(newDocRef, {
       fruitName: inputValue1,
       fruitDefinition: inputValue2
-    }).then(() => {
-      alert('Data saved successfully!');
-
+    }).then( () => {
+      alert("data saved successfully")
     }).catch((error) => {
-      alert('Failed to save data: ' + error.message);
-    });
+      alert("error: ", error.message);
+    })
   }
 
 
   return (
     <div>
-      <input type='text' value={inputValue1}
-      onChange={(e) => setInputValue1(e.target.value)} />
-      <input type='text' value={inputValue2}
-      onChange={(e) => setInputValue2(e.target.value)} /><br />
-      <button onClick={saveData}>Save Data</button>
+
+      <h1>WRITE/HOMEPAGE</h1>
+
+      <input type='text' value={inputValue1} 
+      onChange={(e) => setInputValue1(e.target.value)}/> 
+
+      <input type='text' value={inputValue2} 
+      onChange={(e) => setInputValue2(e.target.value)}/> <br/>
+
+      <button onClick={saveData}>SAVE DATA</button>
+      <br />
+      <br />
+      <br />
+      <button className='button1' onClick={ () => navigate("/updateread")}>GO UPDATE READ</button> <br />
+      <button className='button1' onClick={ () => navigate("/read")}>GO READ PAGE</button>
     </div>
   )
 }
